3833a9c816010ffecdd1bc0d4b9692992f92bd15
Thiago Gariani <thiagogarianiq@gmail.com>
2022-12-15 15:09:08 -0300

    ESTRUTURA DE DOCUMENTAÇÃO

---
+++ commands/attr/attr.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../EXT2/ext2.h"
+#include "../../utils/utils.h"
+
+/**
+ * @brief 
+ * 
+ * @param file 
+ * @param inode 
+ * @param group 
+ * @param super 
+ * @param file_name 
+ */
+void attrCommand(FILE* file, struct ext2_inode inode, struct ext2_group_desc *group, struct ext2_super_block* super, char* file_name);
