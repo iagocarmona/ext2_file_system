56fafddb14d68d98892311332c9a3eaf4692df13
Thiago Gariani <thiagogarianiq@gmail.com>
2022-12-17 15:20:04 -0300

    ADICIONAR

--- commands/pwd/pwd.h
+++ commands/pwd/pwd.h
@@ -1,3 +1,15 @@
+/**
+ * @file pwd.h
+ * @author Iago Ortega Carmona
+ * @author Thiago Gariani Quinto
+ * @author Reginaldo Gregorio de Souza Neto
+ * @brief arquivo de declaração da função que realiza o comando pwd
+ * @version 0.1
+ * @date 2022-12-16
+ * 
+ *
+ */
+
 #pragma once
 
 #include <stdio.h>
@@ -6,4 +18,10 @@
 
 #include "../../StackDirectory/stackDirectory.h"
 
-char* pwdCommand(struct StackDirectory* stackDirectory);
+/**
+ * @brief função que realiza a execução do comando pwd, que exibe o caminho absoluto
+ * 
+ * @param stackDirectory pilha de diretórios
+ * @return char* 
+ */
+char* pwdCommand(StackDirectory* stackDirectory);
